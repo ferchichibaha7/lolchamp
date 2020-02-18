644bc498822699d6c1b373931a854a6ec413727b
Baha <ferchichibaha7@gmail.com>
2020-02-18 20:56:58 +0100

    add riot champions json api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-         Hello friend!
-        </p>
-      </header>
+class App extends Component{
+constructor(){
+  super();
+  this.state={
+    champions:[]
+  }
+}
+
+componentDidMount(){
+  fetch('https://raw.githubusercontent.com/ngryman/lol-champions/master/champions.json')
+  .then(response=>response.json())
+  .then(champs=>this.setState({champions:champs}))
+}
+
+  render(){
+    return(
+      <div className="App">
+     {
+       this.state.champions.map(monster=>
+      <div key={monster.id}>
+<h1>{monster.name}</h1>
+<img src={monster.icon} alt="champIcon"></img>
+<br></br>
+<code>{monster.description}</code>
+<br></br>
+<br></br>
+<br></br>
+</div>
+
+      )
+     }
+
+
+
+
+
+
+
+
+
+
     </div>
-  );
+    )
+  }
 }
 
 export default App;
